9040c29a57bb15fdc5e6e5aa35c9755150e4c1f0
unknown <jiashu.zhu@mooreelite.com>
2019-04-08 14:57:53 +0800

    4/8

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -17,11 +17,6 @@ import card from '@/components/card'
 export default {
   data () {
     return {
-      motto: 'Hello miniprograme',
-      userInfo: {
-        nickName: 'mpvue',
-        avatarUrl: 'http://mpvue.com/assets/logo.png'
-      }
     }
   },
 
